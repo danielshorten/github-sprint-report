3a565475df6fb4dd2bbbaeffb684ffabe97a3f45
Daniel Shorten <daniel.shorten@loknow.com>
2020-09-04 14:18:57 -0600

    Support reporting on multiple repos

--- report.sh
+++ report.sh
@@ -2,15 +2,33 @@
 
 FORMAT=`printf '%%I,\\\"%%t\\\",\\\"%%as\\\",%%S'`
 
-CMD=""
-for SIZE in 1 2 3 5 8 13
+
+########################################################################################################################
+# REPOSITORY REPORT
+########################################################################################################################
+# Append to 'report.csv' a list of issues with sprint and size tags
+function append_repo_report() {
+  CMD=""
+  for SIZE in 1 2 3 5 8 13
+  do
+    if [ ! -z "$CMD" ]; then
+      CMD="$CMD &&"
+    fi
+    CMD="$CMD hub issue -s all -l sprint -L 100 --format \"$FORMAT,$SIZE%n\" -l "size:$SIZE""
+  done
+
+  CMD="{ $CMD; }"
+
+  eval $CMD >> ../report.csv
+}
+
+if [ -f 'report.csv' ]; then
+  rm -i report.csv
+fi
+
+for repo in "$@"
 do
-  if [ ! -z "$CMD" ]; then
-    CMD="$CMD &&"
-  fi
-  CMD="$CMD hub issue -s all -l sprint -L 100 --format \"$FORMAT,$SIZE%n\" -l "size:$SIZE""
+  cd $repo
+  append_repo_report
+  cd ".."
 done
-
-CMD="{ $CMD; }"
-
-eval $CMD > report.csv
